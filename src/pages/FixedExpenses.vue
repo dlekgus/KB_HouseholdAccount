d7e6c8323978c00917ca44ab26c5d1622889e49d
dahyu <dahyun1q@naver.com>
2025-04-06 18:13:06 +0900

    홈 레이아웃, 캘린더, 날짜클릭시 날짜 콘솔 완료료

---
+++ src/pages/FixedExpenses.vue
@@ -0,0 +1,9 @@
+<!-- 이번달 구독료 -->
+<!-- 고정지출 추가 -->
+<!-- 구독 목록 -->
+
+<script setup></script>
+
+<template>
+  <h2>고정지출</h2>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,35 @@
+<!-- 소비 캘린더 -->
+<!-- 이번달 수입/지출 -->
+<!-- 최근 거래 -->
+
+<script setup>
+import Calendar from '@/components/Calendar.vue';
+import HomeLayout from '@/components/HomeLayout.vue';
+import RecentHistory from '@/components/RecentHistory.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+  <div>
+    <HomeLayout>
+      <template v-slot:calendar>
+        <Calendar />
+      </template>
+
+      <template v-slot:history>
+        <RecentHistory></RecentHistory>
+      </template>
+
+      <template v-slot:footer>
+        <Footer />
+      </template>
+
+      <template v-slot:fab>
+        <button class="btn btn-outline">
+          ➕
+          <!-- <font-awesome-icon :icon="['fas', 'circle-plus']" /> -->
+        </button>
+      </template>
+    </HomeLayout>
+  </div>
+</template>
