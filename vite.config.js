a07658dc6573795af5c68d5d400c5167094a0839
MeongW <easymiracle@skuniv.ac.kr>
2025-04-09 19:27:14 +0900

    충돌 해결

--- vite.config.js
+++ vite.config.js
@@ -29,14 +29,4 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://kb-json-server.glitch.me",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
 });
